1087b6a4eda9ab0160f959739dd71e24d54cf5e9
lynxdm <lynxdev32@gmail.com>
2023-09-14 17:21:46 +0000

    inital styling and homepage

---
+++ script.js
@@ -0,0 +1,3 @@
+const key = "b8153e3c30f5a9478f55d8ab9c8ddff9";
+const readAccess =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODE1M2UzYzMwZjVhOTQ3OGY1NWQ4YWI5YzhkZGZmOSIsInN1YiI6IjY0ZmY2YzljZWZlYTdhMDBjMzk2MzZmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CGjCPWVoGOpY0bqOjno6UBwQkWyPiO-rDdPQ8EG7ywI";
