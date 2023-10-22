fc856d8bd56b4a074a9b26a055f8dd6774b4fdeb
lynxdm <lynxdev32@gmail.com>
2023-10-22 14:52:02 +0000

    slider support

--- src/sidebar.js
+++ src/sidebar.js
@@ -2,17 +2,22 @@ const sideBar = document.querySelector("aside");
 const sidebarToggle = document.querySelector(".toggle-sidebar");
 
 sidebarToggle.addEventListener("click", () => {
-  sideBar.classList.toggle("show-sidebar");
-  if (sideBar.classList.contains("show-sidebar")) {
-    document.querySelector("main").style.filter = "brightness(55%)";
-  } else {
-    document.querySelector("main").style.filter = "brightness(100%)";
-  }
+  sideBar.classList.add("show-sidebar");
+  document.querySelector("main").style.filter = "brightness(50%)";
+  document.body.style.position = "fixed";
+  // if (sideBar.classList.contains("show-sidebar")) {
+  //   document.querySelector("main").style.filter = "brightness(50%)";
+  //   document.body.style.position = "fixed";
+  // } else {
+  //   document.querySelector("main").style.filter = "brightness(100%)";
+  //   document.body.style.position = "static";
+  // }
 });
 
 document.addEventListener("mousedown", (e) => {
   if (!sideBar.contains(e.target) && !sidebarToggle.contains(e.target)) {
     sideBar.classList.remove("show-sidebar");
     document.querySelector("main").style.filter = "brightness(100%)";
+    document.body.style.position = "static";
   }
 });
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,20 @@
 const themeToggle = document.querySelector(".theme-toggle label");
+const theme = document.querySelector(".theme");
 
 document.addEventListener("DOMContentLoaded", retrieveTheme);
-// event listener for toggle button
+theme.addEventListener("click", () => {
+  // add theme class to body
+  if (!document.body.classList.contains("dark-theme")) {
+    document.body.classList.add("dark-theme");
+    document.querySelector(".theme-toggle input").checked = true;
+    localStorage.setItem("theme", "dark-theme");
+  } else {
+    document.body.classList.remove("dark-theme");
+    document.querySelector(".theme-toggle input").checked = false;
+    localStorage.removeItem("theme");
+  }
+});
+// // event listener for toggle button
 themeToggle.addEventListener("click", () => {
   // add theme class to body
   document.body.classList.toggle("dark-theme");
