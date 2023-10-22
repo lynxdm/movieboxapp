2b86c884b62ee5bc886e32b15857e8bd596ed9b9
lynxdm <lynxdev32@gmail.com>
2023-10-22 15:52:50 +0000

    add .body-padding

--- src/sidebar.js
+++ src/sidebar.js
@@ -3,7 +3,9 @@ const sidebarToggle = document.querySelector(".toggle-sidebar");
 
 sidebarToggle.addEventListener("click", () => {
   sideBar.classList.add("show-sidebar");
-  document.querySelector(".overlay").style.display="block"
+  document.querySelector(".overlay").style.display = "block";
+  document.body.style.position = "fixed";
+  document.body.classList.add("body-sidebar-padding");
   // document.querySelector("main").style.filter = "brightness(50%)";
   // document.body.style.position = "fixed";
   // if (sideBar.classList.contains("show-sidebar")) {
@@ -18,8 +20,9 @@ sidebarToggle.addEventListener("click", () => {
 document.addEventListener("mousedown", (e) => {
   if (!sideBar.contains(e.target) && !sidebarToggle.contains(e.target)) {
     sideBar.classList.remove("show-sidebar");
+    document.body.classList.remove("body-sidebar-padding");
     document.querySelector(".overlay").style.display = "none";
     // document.querySelector("main").style.filter = "brightness(100%)";
-    // document.body.style.position = "static";
+    document.body.style.position = "static";
   }
 });
